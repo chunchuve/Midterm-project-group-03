7017daaafffdfab5601ebad8de19a1926c428c57
chunchuve <chunchu.ve@northeastern.edu>
2025-10-16 23:34:36 -0600

    set up data models
    set up packages and data model classes

---
+++ Group-1-JJSV-UniversityApp/src/UserAccounts/UserAccount.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UserAccounts;
+
+/**
+ *
+ * @author kal bugrara
+ */
+
+import Persona.Person;
+import Persona.Person;
+import info5100.university.example.workareas.Workarea;
+
+/**
+ *
+ * @author kal bugrara
+ */
+public class UserAccount {
+    Person person;
+    Workarea landingworkarea;
+    
+    public UserAccount(Person p){
+        person = p;
+    }
+    
+}
